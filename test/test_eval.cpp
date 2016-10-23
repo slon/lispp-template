a663cf379f909442d0b06669a9db8c39c3e20e82
Fedor Korotkiy <dartslon@gmail.com>
2016-10-23 19:27:33 +0300

    Add tests for lambda

--- test/test_eval.cpp
+++ test/test_eval.cpp
@@ -5,7 +5,3 @@
 TEST_F(LispTest, Quote) {
     ExpectEq("(quote (1 2))", "(1 2)");
 }
-
-TEST_F(LispTest, Lambda) {
-    ExpectEq("((lambda (x) (+ 1 x)) 5)", "6");
-}
---
+++ test/test_lambda.cpp
@@ -0,0 +1,52 @@
+#include "lisp_test.h"
+
+TEST_F(LispTest, SimpleLambda) {
+    ExpectEq("((lambda (x) (+ 1 x)) 5)", "6");
+}
+
+TEST_F(LispTest, LambdaBodyHasImplicitBegin) {
+    ExpectNoError("(define test (lambda (x) (set! x (* x 2)) (+ 1 x)))");
+    ExpectEq("(test 20)", "41");
+}
+
+TEST_F(LispTest, SlowSum) {
+    ExpectNoError("(define slow-add (lambda (x y) (if (= x 0) y (slow-add (- x 1) (+ y 1)))))");
+    ExpectEq("(slow-add 3 3)", "6");
+    ExpectEq("(slow-add 100 100)", "200");
+}
+
+TEST_F(LispTest, LambdaClosure) {
+    ExpectNoError("(define x 1)");
+
+    ExpectNoError(R"(
+        (define range
+          (lambda (x)
+            (lambda ()
+              (set! x (+ x 1))
+              x)))
+                    )");
+
+    ExpectNoError("(define my-range (range 10))");
+    ExpectEq("(my-range)", "11");
+    ExpectEq("(my-range)", "12");
+    ExpectEq("(my-range)", "13");
+
+    ExpectEq("x", "1");
+}
+
+TEST_F(LispTest, LambdaSyntax) {
+    ExpectSyntaxError("(lambda)");
+    ExpectSyntaxError("(lambda x)");
+    ExpectSyntaxError("(lambda (x))");
+}
+
+TEST_F(LispTest, DefineLambdaSugar) {
+    ExpectNoError("(define (inc x) (+ x 1))");
+    ExpectEq("(inc -1)", "0");
+
+    ExpectNoError("(define (add x y) (+ x y 1))");
+    ExpectEq("(add -10 10)", "1");
+
+    ExpectNoError("(define (zero) 0)");
+    ExpectEq("(zero)", "0");
+}
